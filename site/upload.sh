23494ce1d67f2e94f5b6fc859e88ea2d50bcf0f4
Gayan Perera <gayanper@gmail.com>
2015-04-19 11:14:22 +0530

    Update .travis.yml to support uploading files and fixed the repo relative path.

--- site/upload.sh
+++ site/upload.sh
@@ -4,4 +4,4 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/archive-site.zip?explode=1
+curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/eclipse-themes/$VERSION/archive-site.zip?explode=1
