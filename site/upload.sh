073e7495f96d345cebec1ac28388a890afbf0688
Gayan Perera <gayanper@gmail.com>
2015-04-19 10:59:20 +0530

    Added support for bintray deployment of the update site for p2 repo.

---
+++ site/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+API_KEY=$1
+ARCHIVE=$2
+VERSION=$3
+
+curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/archive-site.zip?explode=1
