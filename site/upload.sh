8c5739be41088086fdc46691ffaca63a02cdc40c
Gayan Perera <gayanper@gmail.com>
2018-01-14 20:16:46 +0530

    added support to publish on build

--- site/upload.sh
+++ site/upload.sh
@@ -5,3 +5,5 @@ ARCHIVE=$2
 VERSION=$3
 
 curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/eclipse-themes/$VERSION/archive-site.zip?explode=1
+curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/publish
+
