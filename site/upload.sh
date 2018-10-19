4181d4692fe1d42d33b7d7ba6a685fc4bdd7336a
gayanper <gayanper@gmail.com>
2018-10-19 22:39:05 +0530

    theme updates and ci pipeline changes
    - fix breadcrumb bar styling for paper dark
    - update upload.sh

--- site/upload.sh
+++ site/upload.sh
@@ -4,6 +4,11 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/eclipse-themes/$VERSION/archive-site.zip?explode=1
+curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
+        -H "X-Bintray-Package:eclipse-themes" \
+        -H "X-Bintray-Version:$VERSION" \
+        -H "X-Bintray-Explode:1" \
+        https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION
+
 curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-themes/$VERSION/publish
 
